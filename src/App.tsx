86aaa3dd420f20b03f259a41f99d06a58db55980
Paul Tanchareon <paul.vtan@live.com>
2020-08-11 17:41:12 +1200

    initial project import

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import {
+  JsonHubProtocol,
+  HubConnectionState,
+  HubConnectionBuilder,
+  LogLevel,
+  HubConnection,
+} from "@microsoft/signalr";
+import "./App.css";
+
+function App() {
+  const [number, setNumber] = useState(Math.round(Math.random() * 10));
+  const connection = new HubConnectionBuilder()
+    .withUrl("https://localhost:44301/hub")
+    .configureLogging(LogLevel.Information)
+    .build();
+  const [hubConnection, setHubConnection] = useState<HubConnection>(connection);
+
+  function handleClick() {
+    const newNumber = getRandomInt(1000);
+    hubConnection.invoke("BroadcastNumber", newNumber);
+  }
+
+  function getRandomInt(max: any) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  useEffect(() => {
+    hubConnection.on("ReceiveMessage", (user, message) => {
+      console.log(user + " " + message);
+    });
+
+    hubConnection.on("ShowUserCounts", (usersCount: any) => {
+      console.log(usersCount);
+    });
+
+    hubConnection.on("UpdateNumber", (num: any) => {
+      console.log("Live: " + num);
+      setNumber(num);
+    });
+
+    hubConnection.start().catch(function (e) {});
+  }, []);
+  return (
+    <>
+      <div>
+        <NumberDisplay num={number} />
+        <button className="button-center" onClick={handleClick}>
+          Click me!
+        </button>
+      </div>
+    </>
+  );
+}
+
+function NumberDisplay(props: any) {
+  return <p className="number-center">{props.num}</p>;
+}
+
+export default App;
